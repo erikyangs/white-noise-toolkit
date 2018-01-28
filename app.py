f4c95c9613d49a5aee05932dbb13e7d4b7fb5470
Erik Yang <erikyangs@gmail.com>
2018-01-28 03:09:49 -0800

    working http get method from random.org

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import urllib.request
 
+# python -m doctest -v app.py
+
 # returns http response from get
 def http_get(url):
     return urllib.request.urlopen(url).read()
@@ -29,6 +31,26 @@ def http_query(base, args):
             http_string += "&"
     return http_string
 
+# creates http request string with arguments num, min, max
+def random_org_url(num, min, max):
+    """
+    >>> random_org_url(10, 1, 6)
+    'https://www.random.org/integers/?num=10&min=1&max=6&col=1&base=10&format=plain&rnd=new'
+    """
+    base_url = "https://www.random.org/integers"
+    http_url = http_query(base_url, [("num", num), ("min", min), ("max", max), ("col", 1), ("base", 10), ("format", "plain"), ("rnd", "new")])
 
-def random_org(min, max, plain):
-    pass
+    return http_url
+
+# calls get request of numbers with args num, min, max; returns list of random numbers
+def get_random_numbers(num, min, max):
+    url = random_org_url(num, min, max)
+    response = http_get(url)
+
+    if response[-1:] == b'\n':
+        response = response[:-1]
+
+    num_string = response.decode('UTF-8')
+
+    return num_string.split("\n")
+
